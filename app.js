dffb6c96838a4d164daa6dd80cff8d292eb1b089
NDROCHKA <andrewkho006@gmail.com>
2025-01-24 21:40:46 +0200

    telit_tajribe

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-console.log("baroud")
-// npm init to intialize
-// npm i "esem el pakage"
-// .gitignore ( and select the files not wanted to be sent)
-// baroudd testt
-// baroudd testt
-// baroudd testt
-// baroudd testt
-// baroudd testt
-// baroudd testt
-// baroudd testt
-// baroudd testt
-// baroudd testt
-// baroudd testt
-// baroudd testt
+import baroud from "./temrinn";
+
+baroud.printt();
